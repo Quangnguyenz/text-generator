ac5694c8cac179e1463c609dfb0650a3113a9f05
Quangnguyenz <81808085+Quangnguyenz@users.noreply.github.com>
2021-11-13 13:27:34 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,14 @@ function App() {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log('hello world')
+    let amount = parseInt(count)
+    if (count <= 0) {
+      amount = 1
+    }
+    if (count > 8) {
+      amount = 8
+    }
+    setText(data.slice(0, amount))
   }
 
   return (
@@ -20,8 +28,13 @@ function App() {
         <button type='submit' className='btn'>generate</button>
       </form>
       <article className='lorem-text'>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum dolore earum impedit, provident blanditiis soluta officiis facilis sunt eius quasi repellat neque asperiores aperiam aspernatur rerum non sint odit vel?</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum dolore earum impedit, provident blanditiis soluta officiis facilis sunt eius quasi repellat neque asperiores aperiam aspernatur rerum non sint odit vel?</p>
+        {console.log(data)}
+
+        {text.map((item, index) => {
+          return (
+            <p key={index}>{item}</p>
+          )
+        })}
       </article>
     </section >
   )
